4c028aa18b9d01d18a344f41bdeba008dd398f68
Muzammal Ghafoor <muzammalghafoor04@gmail.com>
2025-09-20 04:07:16 -0700

    Conatct Us & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,24 @@ import TrustedBy from './components/TrustedBy';
 import Services from './components/Services';
 import OurWork from './components/OurWork';
 import Teams from './components/Teams';
-
+import ContactUs from './components/ContactUs';
+import toast, { Toaster } from 'react-hot-toast';
+import Footer from './components/Footer';
 const App = () => {
 const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
   return (
     
    
     <div className={'dark:bg-black relative'}>
+      <Toaster />
      <Navbar theme={theme} setTheme={setTheme}/>
      <Hero/>
      <TrustedBy/>
      <Services/>
      <OurWork/>
      <Teams/>
+     <ContactUs/>
+     <Footer theme={theme}/>
     </div>
     
   )
